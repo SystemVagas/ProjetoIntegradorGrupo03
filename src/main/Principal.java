b845015dabcdc7969ce4c2f4ca24ebd16eb0855d
Vitor Quagliotto De Souza <vitor.souza5@alunos.senac.sc.br>
2021-10-12 15:01:00 -0300

    Implementação da função menuUsuário

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -1,9 +1,42 @@
 package main;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int opcaoUsuario;
+
+		Scanner leitor = new Scanner(System.in);
+
+		do {
+			menuUsuario();
+			opcaoUsuario = leitor.nextInt();
+
+			if (opcaoUsuario > 2 || opcaoUsuario < 0) {
+				limparTela();
+				System.out.println("Opção inválida!!");
+				System.out.println();
+			}
+
+		} while (opcaoUsuario > 2 || opcaoUsuario < 0);
+
+		leitor.close();
+	}
+
+	public static void limparTela() {
+		for (int i = 0; i < 100; i++) {
+			System.out.println();
+		}
+
+	}
+
+	public static void menuUsuario() {
+
+		System.out.println("Qual usuário deseja iniciar?");
+		System.out.println();
+		System.out.println("1 - Candidato");
+		System.out.println("2 - Recrutador");
 
 	}
 
